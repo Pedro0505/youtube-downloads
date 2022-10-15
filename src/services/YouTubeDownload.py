56d5e5fd534d6267ad99be172cbd63cccf576611
pedro <pedrohrds05@hotmail.com>
2022-10-15 17:24:46 -0300

    fix: fix a video without song error

--- src/services/YouTubeDownload.py
+++ src/services/YouTubeDownload.py
@@ -1,12 +1,15 @@
 import os
+import shutil
+import moviepy.editor as mpe
 
 
 class YouTubeDownload:
     def __init__(self, youtube):
         self.youtube = youtube
+        self.v_name = "video.mp4"
+        self.a_name = "audio.mp3"
 
     def only_song(self, url, path):
-        print('Baixando...')
         yt = self.youtube(url)
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path=path)
@@ -15,7 +18,24 @@ class YouTubeDownload:
         os.rename(out_file, new_file)
 
     def video(self, url, res, path):
-        print('Baixando...')
         yt = self.youtube(url)
-        video_fillter = yt.streams.filter(file_extension='mp4', res=res)
-        video_fillter.first().download(output_path=path)
+
+        video = yt.streams.filter(file_extension='mp4', res=res)
+        video_download = video.first().download(output_path=path)
+        shutil.move(video_download, path + self.v_name)
+
+        audio = yt.streams.filter(only_audio=True)
+        audio_download = audio.first().download(output_path=path)
+        shutil.move(audio_download, path + self.a_name)
+
+        self.merge_video_with_audio(yt.title, path)
+
+    def merge_video_with_audio(self, video_title, path):
+        video = mpe.VideoFileClip(path + self.v_name)
+        audio = mpe.AudioFileClip(path + self.a_name)
+        final = video.set_audio(audio)
+
+        final.write_videofile(path + video_title + '.mp4')
+
+        os.remove(path + self.v_name)
+        os.remove(path + self.a_name)
